e1c4301a4b09fd19a60f7876a911511ebf1a41d5
DuHuaHang <1301934908@qq.com>
2021-06-12 18:41:07 +0800

    测试

---
+++ 01.js
@@ -0,0 +1,28 @@
+// 创建命令: vue create toutiao
+// 1. default(babel,eslint) (集成模式)
+// 进入 Manually select features （手动模式）
+// 2.选择 124567
+// 3.选择2.0x版本
+// 4.no
+// 5.less
+// 6.ESLint + Standard config
+// 7.选择12
+// 8. 1
+// 9.cd toutiao
+// 10.npm run serve
+
+// 删除
+// 进入App.vue 把默认样式结构全部删除
+// 进入router/index.js 把const routes = []路由清空
+// 把views文件夹下的删除，路由都删除了留着没有作用了
+// components文件下的组件删除
+
+// 添加
+// 创建api文件夹，存放接口什么的
+// 创建utiles文件夹，存储工具模块什么的
+// 创建styles文件夹，存放样式
+
+// 加载
+// 在main.js中加载全局样式     加载全局样式:import './styles/index.less'
+
+// 提交到git       git add .   git commit -m '测试'    git push
